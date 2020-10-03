a8e3c45ee9f7dbe253f91d97ff5200f2152eba33
jasvithlani1 <jas.vithlani1@gmail.com>
2020-10-03 20:38:43 +0530

    intital commit

---
+++ lib/model/missed_ingredients.dart
@@ -0,0 +1,15 @@
+class MissedIngredientsModel {
+  final String aisle;
+  final String amount;
+  final String image;
+  final String name;
+  final String original;
+
+  MissedIngredientsModel({
+    this.aisle,
+    this.amount,
+    this.image,
+    this.name,
+    this.original,
+  });
+}
